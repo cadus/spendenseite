95144f202c57421ba5d5021a8044e4ec5236def9
Max Beier <beierm@htw-berlin.de>
2016-12-20 13:55:09 +0100

    Added soft hyphens

--- assets/js/config.js
+++ assets/js/config.js
@@ -13,31 +13,31 @@ window.config = {
       top: '31%',
       earmark: '147181',
       content: '<p>Ein oder mehrere Bauteile auswählen:</p>\
-                <div class="item" data-price="1275"><span class="name">CTG (Cardiotokograph)</span></div>\
+                <div class="item" data-price="1275"><span class="name">CTG (Cardio­tokograph)</span></div>\
                 <div class="item" data-price="1335"><span class="name">Absaugeinheiten</span></div>\
-                <div class="item" data-price="2000"><span class="name">Anaesthisia Infusions Pumps</span></div>\
+                <div class="item" data-price="2000"><span class="name">Anaesthisia In­fusions Pumps</span></div>\
                 <div class="item" data-price="20000"><span class="name">Anaesthesia units</span></div>\
-                <div class="item" data-price="2500"><span class="name">Autoklav für Instrumente und OP-Bekleidung</span></div>',
+                <div class="item" data-price="2500"><span class="name">Autoklav für In­stru­men­te und OP-Be­klei­dung</span></div>',
    }, {
       left: '8%',
       top: '44%',
       earmark: '147182',
       content: '<p>Ein oder mehrere Bauteile auswählen:</p>\
-                <div class="item" data-price="12"><span class="name">Abnabelungsset, 10 Stück</span></div>\
-                <div class="item" data-price="5000"><span class="name">AED (besser richtiger Defi wie LP12, LP15, Corpuls C3)</span></div>\
-                <div class="item" data-price="6"><span class="name">Bluttransfusionsbeutel, 450 ml, 150 Stück</span></div>\
+                <div class="item" data-price="12"><span class="name">Abnabelungs­set, 10 Stück</span></div>\
+                <div class="item" data-price="5000"><span class="name">AED (besser richt­iger Defi wie LP12, LP15, Corpuls C3)</span></div>\
+                <div class="item" data-price="6"><span class="name">Bluttransfusions­beutel, 450 ml, 150 Stück</span></div>\
                 <div class="item" data-price="14000"><span class="name">C-Bogen</span></div>\
                 <div class="item" data-price="50"><span class="name">Spritzen, 2, 5, 10ml, jeweils 500</span></div>\
-                <div class="item" data-price="50"><span class="name">Spritzen, 20 ml, 50 Stück</span></div>',
+                <div class="item" data-price="50"><span class="name">Spritzen, 20ml, 50 Stück</span></div>',
    }, {
       left: '26.6%',
       top: '44%',
       earmark: '147183',
       content: '<p>Ein oder mehrere Bauteile auswählen:</p>\
-                <div class="item" data-price="50"><span class="name">Beatmungsbeutel, 500 und 1600 ml, Mehrweg, </span></div>\
+                <div class="item" data-price="50"><span class="name">Beatmungsbeutel, 500 und 1600 ml, Mehrweg</span></div>\
                 <div class="item" data-price="250"><span class="name">Electric Heater für Neugeborene</span></div>\
                 <div class="item" data-price="20"><span class="name">Handbeatmungsbeutel</span></div>\
-                <div class="item" data-price="400"><span class="name">Infusionssysteme, 400 Stück</span></div>\
+                <div class="item" data-price="400"><span class="name">Infusions­systeme, 400 Stück</span></div>\
                 <div class="item" data-price="80"><span class="name">Pulsoxymeter</span></div>',
    }, {
       left: '83.2%',
@@ -46,9 +46,9 @@ window.config = {
       content: '<p>Ein oder mehrere Bauteile auswählen:</p>\
                 <div class="item" data-price="5"><span class="name">Pupillenleuchten</span></div>\
                 <div class="item" data-price="5"><span class="name">Reflexhammer</span></div>\
-                <div class="item" data-price="1500"><span class="name">sterile OP-Handschuhe, Größen S-L, 600 Stück</span></div>\
+                <div class="item" data-price="1500"><span class="name">sterile OP-Hand­schuhe, Größen S-L, 600 Stück</span></div>\
                 <div class="item" data-price="20"><span class="name">Stethoskope, einfache Ausführung</span></div>\
-                <div class="item" data-price="200"><span class="name">Vollausgestatteter Notfallkoffer, 3 Stück</span></div>',
+                <div class="item" data-price="200"><span class="name">Voll­ausgestatteter Not­fall­koffer, 3 Stück</span></div>',
    }],
 
 };
